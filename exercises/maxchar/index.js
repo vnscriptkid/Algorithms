56d783f2cb1f2c1bcdecd33238cd93df3fed3124
Thanh Nguyen <vnscriptkid@gmail.com>
2021-04-08 10:03:21 +0700

    Solved maxchar problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const dict = {};
+    let output = '', max = 0;
+
+    for (let char of str) {
+        dict[char] = dict[char] ? dict[char] + 1 : 1;
+    }
+
+    for (let char in dict) {
+        if (dict[char] > max) {
+            output = char;
+            max = dict[char];
+        }
+    }
+
+    return output;
+}
 
 module.exports = maxChar;
