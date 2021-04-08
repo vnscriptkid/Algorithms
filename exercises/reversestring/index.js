7450b125d19cfd0a6e98d0dda5885cc6f3fbee9b
Thanh Nguyen <vnscriptkid@gmail.com>
2021-04-08 07:04:56 +0700

    Solved reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// Simplest way
+// '123' => '1' => '21' => '321'
+function reverse(str) {
+    let output = '';
+
+    for (let char of str) {
+        output = char + output;
+    }
+
+    return output;
+}
+
+// Javascript way
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// Recursive way
+// function reverse(str) {
+//     if (str === '') return '';
+//     return reverse(str.substr(1)) + str.charAt(0);
+// }
+
 
 module.exports = reverse;
