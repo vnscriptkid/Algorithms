8c4b025d7518c3d006212918630d22f8dfdeea36
Thanh Nguyen <vnscriptkid@gmail.com>
2021-04-10 13:21:28 +0700

    Solved vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,29 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// 1st way: iterative solution
+// function vowels(str) {
+//     const allVowels = 'aeiou';
+//     let count = 0;
+//     for (let char of str.toLowerCase()) {
+//         if (allVowels.includes(char)) count++;
+//     }
+//     return count;
+// }
+
+// 2nd way: using regex 1
+// function vowels(str) {
+//     let count = 0;
+//     for (let char of str) {
+//         if (/[aeiou]/gi.test(char)) count++;
+//     }
+//     return count;
+// }
+
+// 3nd way: using regex 2
+function vowels(str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
