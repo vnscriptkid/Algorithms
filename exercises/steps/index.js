c730da56e36d94bb6034e0f92ab90e9ee35f8a45
Thanh Nguyen <vnscriptkid@gmail.com>
2021-04-10 13:00:18 +0700

    Solved steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,46 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// 1st way
+// function multiplyChar(char, times) {
+//     return Array.from(new Array(times)).map(() => char).join('');
+// }
+
+
+// function steps(n) {
+//     for (let row = 1; row <= n; row++) {
+//         console.log(multiplyChar('#', row) + multiplyChar(' ', n - row));
+//     }
+// }
+
+// 2nd way
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let line = '';
+//         for (let col = 0; col < n; col++) {
+//             if (col <= row) {
+//                 line += '#';
+//             } else {
+//                 line += ' ';
+//             }
+//         }
+//         console.log(line);      
+//     }
+// }
+
+
+
+// 3rd way: Using recursion
+function steps(n, row = 0, line = '') {
+    if (row === n) return;
+
+    if (line.length === n) {
+        console.log(line);
+        steps(n, row + 1);
+    } else {
+        line = line.length <= row ? line + '#' : line + ' ';
+        steps(n, row, line)
+    }
+}
 
 module.exports = steps;
