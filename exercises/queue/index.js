0299a668a3f83b23fb859540624bdfa21ee530e5
Thanh Nguyen <vnscriptkid@gmail.com>
2021-04-11 18:13:06 +0700

    Solved queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,18 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.queue = [];
+    }
+
+    add(value) {
+        this.queue.unshift(value);
+    }
+
+    remove() {
+        return this.queue.pop();
+    }
+}
 
 module.exports = Queue;
