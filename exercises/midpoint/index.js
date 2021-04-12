e034a59363c4ee0bbaa42c9582afecdcee9337f3
Thanh Nguyen <vnscriptkid@gmail.com>
2021-04-12 16:42:04 +0700

    Solved midpoint

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -12,6 +12,35 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+// 1st way: my original solution
+// function midpoint(list) {
+//     if (list.head === null || list.head.next === null) {
+//         return list.head;
+//     }
+
+//     let slowPointer = list.head;
+//     let fastPointer = slowPointer.next;
+
+//     while (true) {
+//         if (!fastPointer || !fastPointer.next)
+//             break;
+//         slowPointer = slowPointer.next;
+//         fastPointer = fastPointer.next.next;
+//     }
+//     return slowPointer;
+// }
+
+// 2nd way: shorter one
+function midpoint(list) {
+    let slowPointer = list.getFirst();
+    let fastPointer = list.getFirst();
+
+    while (fastPointer.next && fastPointer.next.next) {
+        slowPointer = slowPointer.next;
+        fastPointer = fastPointer.next.next;
+    }
+
+    return slowPointer;
+}
 
 module.exports = midpoint;
