fa792059ce3feab7bad512c08aba42d89f4f6642
Thanh Nguyen <vnscriptkid@gmail.com>
2021-04-14 01:25:18 +0700

    Solved mergeSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -35,7 +35,14 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+    if (arr.length === 1) return arr;
 
+    let middle = Math.floor(arr.length / 2);
+
+    const leftArr = arr.slice(0, middle);
+    const rightArr = arr.slice(middle);
+
+    return merge(mergeSort(leftArr), mergeSort(rightArr));
 }
 
 function merge(left, right) {
