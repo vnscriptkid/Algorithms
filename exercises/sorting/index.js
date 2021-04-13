80462350be3319845d8028f94d69ee434d1f27fa
Thanh Nguyen <vnscriptkid@gmail.com>
2021-04-14 00:50:52 +0700

    Solved selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -20,7 +20,18 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            swap(arr, i, minIndex);
+        }
+    }
+    return arr;
 }
 
 function mergeSort(arr) {
