0bdbf6817227f2e3b15c17e6837725cf31879b45
Thanh Nguyen <vnscriptkid@gmail.com>
2021-04-13 23:44:57 +0700

    Solved bubble sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,8 +1,22 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(arr) {
+function swap(arr, i, j) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
 
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+
+    return arr;
 }
 
 function selectionSort(arr) {
