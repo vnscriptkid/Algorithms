568deb9ed327b64cd91a2cfb9b9551bd3c159d47
Thanh Nguyen <vnscriptkid@gmail.com>
2021-04-14 01:07:50 +0700

    Solved merge

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -39,7 +39,17 @@ function mergeSort(arr) {
 }
 
 function merge(left, right) {
+    const result = [];
 
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    return [...result, ...left, ...right]
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
